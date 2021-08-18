37818b309ba67036b125b99ee9dfdbc675fd5005
Juanmanato <natobily20sas@yahoo.com.ar>
2021-08-18 19:25:42 -0300

    Juanmanato
    subiendo proyecto individual

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen(3000, 
+    console.log('Servidor levantado'))
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname,"/views/index.html"))
+
+})
+
+
+app.get('/register.html', function (req, res) {
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+   
+})
+
+app.get("/login.html", function (req, res) {
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+   
+})
