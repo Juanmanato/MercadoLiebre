170b6743c47c2e019fa7e77b74195c2c3ecfcd55
Juanmanato <natobily20sas@yahoo.com.ar>
2021-08-18 20:05:33 -0300

    Juanmanato
    subiendo gitignore

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.listen(3000, 
-    console.log('Servidor levantado'))
+app.listen(precess.env.PORT || 3000, function() {
+    console.log('Servidor levantado')})
 
 app.use(express.static(path.join(__dirname, '/public')))
 
